869d995147530f5de9915a0646616a3662b356f1
Suchitha <suchitha.channa@gmail.com>
2025-03-07 00:41:04 +0000

    Add launch file for wall follower and safety controller

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = "safety_controller"
 
@@ -9,6 +11,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml", "safety_controller/params.yaml"]),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
